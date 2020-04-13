73cf1448288b3650639d9078287bbe000e18ae21
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-14 00:55:34 +0530

    Easy

--- C/Pointers in C/Solution.c
+++ C/Pointers in C/Solution.c
@@ -3,16 +3,16 @@
 
 void update(int *a,int *b) 
 {
-    *a = *a + *b;
-    *b = abs(*a - (2 * *b));
+      	*a = *a + *b;
+    	*b = abs(*a - (2 * *b));
 }
 
 int main() 
 {
-    int a, b;
-    int *pa = &a, *pb = &b;
-    scanf("%d %d", &a, &b);
-    update(pa, pb);
-    printf("%d\n%d", a, b);
-    return 0;
+    	int a, b;
+    	int *pa = &a, *pb = &b;
+    	scanf("%d %d", &a, &b);
+    	update(pa, pb);
+    	printf("%d\n%d", a, b);
+    	return 0;
 }

1efb7ad984c751574fb00de55e6caf5d15cbff44
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-13 22:17:50 +0530

    Easy

---
+++ Mathematics/Cutting Paper Squares/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		long n = s.nextLong();
+		long m = s.nextLong();
+		System.out.println(n * m - 1);
+		s.close();
+	}
+}

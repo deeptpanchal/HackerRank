f3616ca5a35398cf9ecf95bb1e3fd5e5958df951
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-13 23:32:25 +0530

    Easy

---
+++ Mathematics/Number Groups/Solution.java
@@ -0,0 +1,13 @@
+import java.math.*;
+import java.util.*;
+
+public class Solution 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		BigInteger b = s.nextBigInteger();
+		System.out.println(b.pow(3));
+		s.close();
+	}
+}

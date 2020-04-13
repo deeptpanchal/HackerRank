4e16d6cd62328cde4f6508b0045917ce91c9f6ac
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-14 00:34:26 +0530

    Easy

---
+++ C/Playing With Characters/Solution.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char c1;
+    scanf("%c",&c1);
+    char c2[100];
+    scanf("%s",c2);
+    char t;
+    scanf("%c",&t);
+    char c3[' '];
+    scanf("%[^\n]%*c",c3);
+    printf("%c",c1);
+    printf("\n");
+    printf("%s",c2);
+    printf("\n");
+    printf("%s",c3);
+    return 0;
+}

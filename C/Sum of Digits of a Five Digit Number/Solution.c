529c1e9ca42caaa2c94ee2ccc3c3dcf3278b3b8f
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-14 00:56:18 +0530

    Easy

--- C/Sum of Digits of a Five Digit Number/Solution.c
+++ C/Sum of Digits of a Five Digit Number/Solution.c
@@ -5,13 +5,13 @@
 
 int main() 
 {
-    int n;
-    scanf("%d", &n);
-    int sum = 0 ;
-    for(int i = n ;i>0;i=i/10)
-    {
-        sum=sum+i%10;
-    }
-    printf("%d",sum);
-    return 0;
+    	int n;
+    	scanf("%d", &n);
+    	int sum = 0 ;
+    	for(int i = n ;i>0;i=i/10)
+    	{
+        	sum=sum+i%10;
+    	}
+    	printf("%d",sum);
+    	return 0;
 }

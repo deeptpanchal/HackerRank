37267a37fb2aca65a8ed1a8f719340ddf582ea51
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-14 00:54:06 +0530

    Easy

--- C/Functions in C/Solution.c
+++ C/Functions in C/Solution.c
@@ -2,26 +2,26 @@
 
 int max_of_four(int a, int b, int c, int d) 
 {
-    if (a >= b && a >= c && a >= d) 
-    {
-      return a;
-    }
-    if (b >= a && b >= c && b >= d) 
-    {
-      return b;
-    }
-    if (c >= a && c >= b && c >= d) 
-    {
-      return c;
-    }
-    return d;
+    	if (a >= b && a >= c && a >= d) 
+    	{
+      		return a;
+    	}
+    	if (b >= a && b >= c && b >= d) 
+    	{
+      		return b;
+    	}
+    	if (c >= a && c >= b && c >= d) 
+    	{
+      		return c;
+    	}
+    	return d;
 }
 
 int main() 
 {
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
-    return 0;
+    	int a, b, c, d;
+    	scanf("%d %d %d %d", &a, &b, &c, &d);
+    	int ans = max_of_four(a, b, c, d);
+    	printf("%d", ans);
+    	return 0;
 }

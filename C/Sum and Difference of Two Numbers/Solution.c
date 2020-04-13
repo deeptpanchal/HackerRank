90a33979d841b22a69a2328f2af8d3131c76f5f4
Deept Panchal <49604480+deeptpanchal@users.noreply.github.com>
2020-04-14 00:47:11 +0530

    Easy

--- C/Sum and Difference of Two Numbers/Solution.c
+++ C/Sum and Difference of Two Numbers/Solution.c
@@ -6,15 +6,15 @@
 int main()
 {
 	int n1;
-    int m1;
-    float n2;
-    float m2;
-    scanf("%d",&n1);
-    scanf("%d",&m1);
-    scanf("%f", &n2);
-    scanf("%f", &m2);
-    printf("%d %d",n1+m1,n1-m1);
-    printf("\n");
-    printf("%.1f %.1f",n2+m2,n2-m2);
-    return 0;
+    	int m1;
+    	float n2;
+    	float m2;
+    	scanf("%d",&n1);
+    	scanf("%d",&m1);
+    	scanf("%f", &n2);
+    	scanf("%f", &m2);
+    	printf("%d %d",n1+m1,n1-m1);
+    	printf("\n");
+    	printf("%.1f %.1f",n2+m2,n2-m2);
+    	return 0;
 }
